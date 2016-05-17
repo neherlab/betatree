463820b386bc2628f3adb67675be634482ea2261
Richard <richard.neher@tuebingen.mpg.de>
2016-05-17 17:33:10 +0200

    added yule tree feature

--- betatree/betatree.py
+++ betatree/betatree.py
@@ -9,7 +9,7 @@ import random as rand
 import scipy.special as sf
 from Bio import Phylo
 
-class betatree(object):    
+class betatree(object):
     '''
     class that simulates a beta coalescent tree
     parameters:
@@ -19,6 +19,7 @@ class betatree(object):
     def __init__(self, sample_size, alpha=2):
         self.alpha=alpha
         self.n = sample_size
+        self.yule=False
         # auxillary arrays
         self.k = np.arange(self.n+1)
         if alpha==1:
@@ -26,7 +27,7 @@ class betatree(object):
             self.inv_kkp1 = self.inv_k/np.arange(2,self.n+3)
             self.cum_sum_inv_kkp1 = np.array([0]+np.cumsum(self.inv_kkp1).tolist())
         elif alpha>1 and alpha<2:
-            self.normalizer = 1.0/sf.gamma(alpha)/sf.gamma(2-alpha) 
+            self.normalizer = 1.0/sf.gamma(alpha)/sf.gamma(2-alpha)
             self.gamma_ratiom = np.exp(sf.gammaln(self.k-self.alpha) - sf.gammaln(self.k+1))
             self.gamma_ratiop = np.exp(sf.gammaln(self.k+self.alpha) - sf.gammaln(self.k+1))
 
@@ -35,7 +36,7 @@ class betatree(object):
         constructs the blocks that are to be merged, each leave corresponds
         to a BioPython clade object
         '''
-        self.blocks = [ Phylo.BaseTree.Clade(name=str(i), branch_length=0) 
+        self.blocks = [ Phylo.BaseTree.Clade(name=str(i), branch_length=0)
                       for i in range(self.n)]
 
     def coalescence_event(self):
@@ -52,7 +53,7 @@ class betatree(object):
             clade.branch_length+=waiting_time
 
         #randomly pick some blocks (there are (p choose k) possibilities)
-        merging_blocks = rand.sample(self.k[:len(self.blocks)], merger_size) 
+        merging_blocks = rand.sample(self.k[:len(self.blocks)], merger_size)
         self.merge_clades(merging_blocks)
 
     def merge_clades(self, merging_blocks):
@@ -60,7 +61,7 @@ class betatree(object):
         creates a new clade whose children are the merging blocks
         '''
         # instantiate at Clade object with children given by the merging blocks
-        new_clade = Phylo.BaseTree.Clade(clades = [self.blocks[i] 
+        new_clade = Phylo.BaseTree.Clade(clades = [self.blocks[i]
                                                    for i in merging_blocks])
         # set the branch length to that of the children
         new_clade.branch_length = self.blocks[merging_blocks[0]].branch_length
@@ -86,14 +87,17 @@ class betatree(object):
 
     def waiting_time(self):
         '''
-        returns the waiting time to the next merger. 
+        returns the waiting time to the next merger.
         '''
         b=len(self.blocks)
         if self.alpha==1:   #the BSC merger rate
             # the BSC merger rate is simply b-1 = 1 / (sum_k 1/k(k-1))
-            dt = np.random.exponential(1.0/(b-1)) 
+            dt = np.random.exponential(1.0/(b-1))
         elif self.alpha==2: #the Kingman merger rate
-            dt = np.random.exponential(2.0/b/(b-1)) 
+            if self.yule:
+                dt = np.random.exponential(1.0/b)
+            else:
+                dt = np.random.exponential(2.0/b/(b-1))
         else: # the general beta coalescent case
             rate = b*(self.gamma_ratiom[2:b+1]*self.gamma_ratiop[b-2::-1]).sum()*self.normalizer
             dt = np.random.exponential(1.0/rate) #this product gives the Beta coalescent merger rate
@@ -132,7 +136,7 @@ class betatree(object):
         #while the whole tree is not merged yet
         while len(self.blocks) != 1:
             self.coalescence_event()
-        
+
         self.clean_up_subtree(self.blocks[0])
         self.BioTree = Phylo.BaseTree.Tree(root=self.blocks[0])
 
@@ -147,6 +151,16 @@ if __name__=='__main__':
     Phylo.draw(myT.BioTree, label_func=lambda x:None)
     plt.title('Kingman: alpha=2')
     plt.savefig('example_trees/kingman.pdf')
+
+    # alpha=2 -> Yule tree
+    myT = betatree(100,alpha = 2)
+    myT.yule=True
+    myT.coalesce()
+    myT.BioTree.ladderize()
+    Phylo.draw(myT.BioTree, label_func=lambda x:None)
+    plt.title('Kingman: alpha=2')
+    plt.savefig('example_trees/kingman.pdf')
+
 
     # alpha=1 -> Bolthausen-Sznitman coalescent tree
     myT = betatree(100,alpha = 1)
